c1445006b545cb4df742fb65447005a374695797
Brian Rosengarten <barosengarten@gmail.com>
2019-01-24 21:36:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,17 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name})")
+    column_names = []
+
+    table_columns.each do |col|
+      column_names << col["name"]
+    end
+
+    column_names.compact
+  end
+
 end
